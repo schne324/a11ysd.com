5f2e621d31ed60ee12c373ce665a5e8d0644de6b
Harris Schneiderman <harris.schneiderman@deque.com>
2019-03-27 14:18:54 -0700

    chore: gets initial coming soon page up

---
+++ README.md
@@ -0,0 +1,5 @@
+# a11ySD.com
+
+> website for San Diego Accessibility & Inclusive Design meetup
+
+This project was bootstrapped by [bollireact](https://github.com/schne324/bollireact)
---
+++ src/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <h1>a11ySD</h1>
+    <h2>San Diego Accessibility & Inclusive Design</h2>
+    <p>
+      Website coming soon! For now, head to the{' '}
+      <a href="https://www.meetup.com/a11ySD/">a11ySD meetup page</a>
+    </p>
+  </div>
+);
+
+App.displayName = 'App';
+export default App;
