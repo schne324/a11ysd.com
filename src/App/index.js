faba7bcd42c3a247f64d0f89f4bf1cbe8f6033ff
Harris Schneiderman <harris.schneiderman@deque.com>
2019-03-27 14:59:42 -0700

    fix: adds main

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const App = () => (
-  <div className="App">
+  <main className="App">
     <h1>a11ySD</h1>
     <h2>San Diego Accessibility & Inclusive Design</h2>
     <p>
       Website coming soon! For now, head to the{' '}
       <a href="https://www.meetup.com/a11ySD/">a11ySD meetup page</a>
     </p>
-  </div>
+  </main>
 );
 
 App.displayName = 'App';
