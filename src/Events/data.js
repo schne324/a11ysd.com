e1de90efac0b374c43414382c9a4f256a9c337a2
Harris Schneiderman <harris.schneiderman@deque.com>
2023-03-15 17:39:02 -0700

    Update data.js

--- src/Events/data.js
+++ src/Events/data.js
@@ -1,8 +1,4 @@
 export default {
-  '04/13/2022': {
-    link: 'https://www.meetup.com/a11ysd/events/292163612/',
-    talks: []
-  },
   '02/23/2022': {
     link: 'https://www.meetup.com/a11ysd/events/290430407/',
     liveStream: 'https://www.youtube.com/embed/6Y3-dJC51TQ',
